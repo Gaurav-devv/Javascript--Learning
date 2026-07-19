d39466d291c04e8cfa558e36366f7afc22a85b88
Gaurav Tiwari <cs.gauravtiwari@gmail.com>
2026-07-19 20:27:34 +0530

    Password checker

--- 05-Building_games/building_games.js
+++ 05-Building_games/building_games.js
@@ -1,20 +1,37 @@
-// NUMBER KNOCK GAME(ASK THE USER TO KEEP GUESSING THE NUMBER UNTIL THE USER
-//ENTERS CORRECT GUESS.)
+// // NUMBER KNOCK GAME(ASK THE USER TO KEEP GUESSING THE NUMBER UNTIL THE USER
+// //ENTERS CORRECT GUESS.)
 
-let computerNumber = 6;
-let guess;
-while (guess !== computerNumber) {
-  guess = Number(prompt("Enter correct number:"));
+// let computerNumber = 6;
+// let guess;
+// while (guess !== computerNumber) {
+//   guess = Number(prompt("Enter correct number:"));
 
-  if (isNaN(guess)) {
-    alert("Number is invalid, please try again!");
-  } else if (guess < computerNumber) {
-    alert("Number is too low!, please try againn!");
-  } else if (guess > computerNumber) {
-    alert("Number is too big!,please try again");
-  } else {
-    alert("Congratulation!... Your guess is correct");
-  }
-}
+//   if (isNaN(guess)) {
+//     alert("Number is invalid, please try again!");
+//   } else if (guess < computerNumber) {
+//     alert("Number is too low!, please try againn!");
+//   } else if (guess > computerNumber) {
+//     alert("Number is too big!,please try again");
+//   } else {
+//     alert("Congratulation!... Your guess is correct");
+//   }
+// }
 
 //SIMPLE PASSWORD CHECKER[FIXED ATTEMPTS]
+
+let password = "alpha";
+let noOfAttempt = 0;
+let userPassword = null;
+
+while (password !== userPassword && noOfAttempt < 3) {
+  userPassword = prompt("Enter password:");
+  noOfAttempt++;
+
+  if (password == userPassword) {
+    alert("Congratulations you password is correct");
+  } else if (noOfAttempt >= 3) {
+    alert("Your attempt is over");
+  } else {
+    alert("Your password is in correct, please try again");
+  }
+}
